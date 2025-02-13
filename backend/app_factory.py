1b0f57742008bdb3a3a4d0193fe406229d3908a3
Vincenzo140 <vincenzo.amendola141@gmail.com>
2025-02-13 01:34:10 -0500

    refactor: remove some useless content

--- backend/app_factory.py
+++ backend/app_factory.py
@@ -2,44 +2,18 @@ from flask import Flask
 from flask_restx import Api, Resource
 from logger.logging import AppLogger
 
-# Criação do logger
 logger = AppLogger().get_logger()
 
-# Função que cria o aplicativo Flask
 def create_app() -> Flask:
-    # Inicializando a aplicação Flask
     app = Flask(__name__)
 
-    # Inicializando a API com Swagger
-    api = Api(app, 
-              title="Flask-ml-dashboard",
-              version="1.1.0",
-              description="Uma simples API sobre saudação e soma",
-              contact={
-                  "name": "Vincenzo Amendola",
-                  "github": "https://github.com/Vincenzo140",
-                  "email": "vincenzo.amendola141@gmail.com",
-                  "linkedin": "https://www.linkedin.com/in/vincenzo-amendola-9aab38264/",
-              },
-              swagger_ui_parameters={
-                  "syntaxHighlight.theme": "monokai",
-                  "layout": "BaseLayout",
-                  "filter": True,
-                  "tryItOutEnabled": True,
-                  "onComplete": "Ok",
-                  "docExpansion": "full",
-                  "showExtensions": True,
-                  "apisSorter": "alpha",
-                  "supportedSubmitMethods": ["get", "post"],
-                  "defaultModelsExpandDepth": 0,
-              })
 
-    # Importando as rotas reais
+    # Importando as routes reais
     try:
         from routes import saudacao as greet_route  # Importa a rota de saudação
         from routes import soma as sum_route  # Importa a rota de soma
         
-        # Registrando as Blueprints
+        # Registrando as Blueprints do flaks
         app.register_blueprint(greet_route.greet)
         app.register_blueprint(sum_route.sum)
         
